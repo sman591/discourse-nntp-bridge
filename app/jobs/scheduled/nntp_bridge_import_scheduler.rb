64e07b5a253cd97b6026ec645ee61ee70e858224
Stuart Olivera <stuart@stuartolivera.com>
2016-03-31 16:57:28 -0400

    Prevent snowballing of failed importers
    Resolves #13

--- app/jobs/scheduled/nntp_bridge_import_scheduler.rb
+++ app/jobs/scheduled/nntp_bridge_import_scheduler.rb
@@ -6,8 +6,21 @@ module Jobs
       newsgroups = CategoryCustomField.where(name: "nntp_bridge_newsgroup").pluck(:value)
 
       newsgroups.each do |newsgroup|
-        next if Jobs::NntpBridgeImporter.is_active? newsgroup
+        next if Jobs::NntpBridgeImporter.is_active? newsgroup || importer_queued? newsgroup
         Jobs.enqueue(:nntp_bridge_importer, newsgroup: newsgroup)
+      end
+    end
+
+    def self.importer_queued?(newsgroup)
+      queries = [
+        Proc.new { Sidekiq::Queue.new },
+        Proc.new { Sidekiq::RetrySet.new }
+      ]
+      queries.any? do |query|
+        query.call.any? do |job|
+          job.klass == 'Jobs::NntpBridgeImporter' &&
+            job.args[0]["newsgroup"] == newsgroup
+        end
       end
     end
   end
