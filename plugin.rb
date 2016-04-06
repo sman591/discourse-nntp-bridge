534f8559d868c6f9a8604c34b11b239c6332b2e9
Stuart Olivera <stuart@stuartolivera.com>
2016-04-06 14:21:51 -0400

    Version bump: 0.1.5

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-nntp-bridge
 # about: Discourse plugin to keep NNTP & Discourse in sync
-# version: 0.1.4
+# version: 0.1.5
 # authors: Stuart Olivera
 # url: https://github.com/sman591/discourse-nntp-bridge
 
