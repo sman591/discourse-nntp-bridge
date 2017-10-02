a9cd4f952e4971f33ca752d9c0c09905a10d92e6
Stuart Olivera <stuart@stuartolivera.com>
2017-10-01 22:33:34 -0400

    Support Rails 5 for ActiveRecord migrations

--- db/migrate/20160215005444_create_nntp_post_associations.rb
+++ db/migrate/20160215005444_create_nntp_post_associations.rb
@@ -1,4 +1,4 @@
-class CreateNntpPostAssociations < ActiveRecord::Migration
+class CreateNntpPostAssociations < ActiveRecord::Migration[4.2]
   def change
     create_table :discourse_nntp_bridge_nntp_posts do |t|
       t.belongs_to :post, index: true
